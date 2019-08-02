16a286cbbedf362050404810a957398170e297ce
thousand7274 <thousand7274@gmail.com>
2019-08-02 16:00:43 +0900

    削除ボタン追加
    リストを削除できるように削除ボタンを追加しました

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 // 上から読まれます！！！
 
-console.log(window.localStorage);
+// console.log(window.localStorage);
 
 let data = [];//もしローカルストレージにデータがなかったらこっちが採用される
 
@@ -38,6 +38,31 @@ function createDOM(value){
   let list = document.createElement('li');
   // liタグ内に値を追加
   list.textContent = value;
+
+  let button = document.createElement('button');
+  button.textContent = '削除';
+  // console.log(button);
+  // ボタンを出力リストの後ろにつく
+  list.appendChild(button);
+  button.addEventListener('click', function(){
+    this.parentNode.remove();//要素を削除
+    // console.log(this.parentNode.textContent)
+    //ストレージ内のデータを探すのはindexOf一致すれば削除できる
+    // 配列の中でどこにあるか探すindexOf
+    // 配列の中の一致する項目を探す
+    // spliceで配列を削除
+    // innerHTMLにすると意図しないデザインに変更されてしまうから使うのは良くない
+    // htmlをなるべくデータとして入れないほうが安全
+    // アラートとか変なサイトを仕込まれたら困るから
+    // 最近のブラウザはフォームにjsやcssを追加できないようになっている
+    data.splice(data.indexOf(this.parentNode.textContent.slice( 0,-2)), 1);
+    // Element.slice(0,-2)「削除」の文字が出てしまうからこれで消す-2の部分は文字で値が変わる
+    // 最後から二文字目ってことマイナスしなかったら前からマイナスしたら後ろから
+  // console.log(data)
+  // データを保存
+  localStorage.setItem('task', JSON.stringify(data));
+
+  })
   // 親要素ulのid=listに子要素として追加
   document.getElementById('list').appendChild(list);
 
