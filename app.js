d622ee6dbd7baa776247af12518326b1fe3ae4e2
thousand7274 <thousand7274@gmail.com>
2019-08-02 14:13:34 +0900

    インテンド修正、コメント追加
    jsとcssのインテンドを修正、jsにコメントを追加しました。

--- app.js
+++ app.js
@@ -3,27 +3,29 @@
 console.log(window.localStorage);
 
 let data = [];
+
 document.getElementById('add')
 .addEventListener('click',
 
   function(){
 
     data.push(document.getElementById('task').value);
-
     createDOM(document.getElementById('task').value);
-
     localStorage.setItem('task', JSON.stringify(data));
+
   }
 );
-// forの前に置かないとできない
-data = JSON.parse(localStorage.getItem('task'));
+
+data = JSON.parse(localStorage.getItem('task'));// forの前に置かないとできない
 
 for (const value of data) {
   createDOM(value);
 }
 
 function createDOM(value){
+
   let list = document.createElement('li');
   list.textContent = value;
   document.getElementById('list').appendChild(list);
+
 }
